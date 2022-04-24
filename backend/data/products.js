9edfa240a39a7201e58f2745eb3d4a7fafde4f0f
sohan lokula s3841218 <s3841218@student.rmit.edu.au>
2022-04-24 12:46:20 +1000

    third commit

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,57 +1,57 @@
 const products = [
     {
-      name: "PlayStation 5",
+      name: "Blue Shirt",
       imageUrl:
-        "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+        "https://images.unsplash.com/photo-1589310243389-96a5483213a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       description:
-        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
-      price: 499,
+        "This amazing blue shirt is made by the wonderful creators in Sri Lanka who also make shirts and t-shirts for some of the biggest brands in the world. Using the same premium quiality material you sure will feel luxorious wearing this.",
+      price: 20,
       countInStock: 15,
     },
     {
-      name: "Iphone 12",
+      name: "Black T-Shirt",
       imageUrl:
-        "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+        "https://images.unsplash.com/photo-1571455786673-9d9d6c194f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       description:
-        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-      price: 1099,
+        "Created in in the foothills of the Himalayas, This Nepalese family run a clothing business from their home. This black t-shirt is made with a special yarn and ink only found in Nepal",
+      price: 25,
       countInStock: 10,
     },
     {
-      name: "Cannon EOS-1D",
+      name: "Designer Jacket",
       imageUrl:
-        "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1619102814948-e164e584cf0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       description:
-        "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
-      price: 1300,
+        "Desgined by the world famous suit designer Tom Cord, You are sure to get your bang for your buck with this Jacket",
+      price: 130,
       countInStock: 5,
     },
     {
-      name: "Amazon Alexa",
+      name: "Yellow Clothes set",
       imageUrl:
-        "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+        "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=840&q=80",
       description:
-        "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
+      "With this warm and fuzzy yellow set, that includes a hoodies and a pant say bye to the winters you hate",
       price: 50,
       countInStock: 25,
     },
     {
-      name: "Audio Technica Headphones",
+      name: "Black Hoodie",
       imageUrl:
-        "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1572495673508-62a6b369c380?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       description:
-        "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
-      price: 233,
+        "This thick black hoodie will keep your fashion style up. Comes with a free black hat",
+      price: 25,
       countInStock: 4,
     },
     {
-      name: "JBL FLIP 4",
+      name: "Grey Hoodie",
       imageUrl:
-        "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+        "https://images.unsplash.com/photo-1609885654455-f81e5a682c87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       description:
-        "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
-      price: 140,
-      countInStock: 10,
+      "Dont like the black hoodie? No worries, Say hello to this new premium gildan grey hoodie",
+      price: 25,
+      countInStock: 1,
     },
   ];
   
--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -5,12 +5,12 @@ const CartItem = () => {
     return (
         <div className="cartitem">
             <div className="cartitem__image">
-            <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" />
+            <img src="https://images.unsplash.com/photo-1571455786673-9d9d6c194f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt='black shirt'/>
             </div>
             <Link to={`/product/${111}`} className="cartitem__name">
-                <p>Product 1</p>
+                <p>Black T-Shirt</p>
             </Link>
-            <p className="cartitem__price">$4.99</p>
+            <p className="cartitem__price">$25</p>
 
             <select className="cartitem__select">
                         <option value="1">1</option>
