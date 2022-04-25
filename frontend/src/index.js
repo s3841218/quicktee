f0fed9af080ccb08a67b6e0bcdcc932333d27042
s3841218 <63080339+s3841218@users.noreply.github.com>
2022-04-25 14:55:09 +1000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,5 @@
+// Rensburg, L., 2020. MERN e-commerce build. [video] Available at: <https://www.youtube.com/watch?v=0divhP3pEsg&t=3215s> [Accessed 12 April 2022].
+// Code adapted from Lloyd Rensburg
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
